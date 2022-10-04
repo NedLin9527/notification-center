c014eac9b5236989a76d9187036052490e73dd7f
Tony Yang <long0426@gmail.com>
2022-10-04 09:35:18 +0800

    build 3 kafka

--- notification-center/src/main/java/com/cdfholding/notificationcenter/kafka/KafkaProducerConfiguration.java
+++ notification-center/src/main/java/com/cdfholding/notificationcenter/kafka/KafkaProducerConfiguration.java
@@ -5,6 +5,7 @@ import com.cdfholding.notificationcenter.dto.AllowedUserMailRequest;
 import com.cdfholding.notificationcenter.serialization.JsonSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -21,10 +22,12 @@ public class KafkaProducerConfiguration {
 //  public ProducerFactory<String, AllowedUserApplyRequest> producerFactory() {
   public ProducerFactory<String, ?> producerFactory() {
     Map<String, Object> props = new HashMap<>();
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.156.63:29092,192.168.156.63:29093,192.168.156.63:29094");
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.156.239:29092");
+//    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.156.63:29092,192.168.156.63:29093,192.168.156.63:29094");
+//    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.156.239:29092");
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:29092,127.0.0.1:29093,127.0.0.1:29094");
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 2);
 
     return new DefaultKafkaProducerFactory<>(props);
   }
